31d95001b96db4ffec6149ea824ca47f67231373
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-03-08 17:43:07 -0300

    Aula 19 - Paginacao de projetos

--- app/Validators/User.js
+++ app/Validators/User.js
@@ -15,29 +15,6 @@ class StoreUser {
     }
   }
 
-  /*
-  Caso os erros de validação não fossem tratados no ExceptionHandler
-
-  async fails (errorMessages) {
-    return this.ctx.response.send(errorMessages)
-  }
-  */
-
-  /*
-  para mensagens não internacionalizadas
-
-  get messages () {
-    return {
-      'username.required': 'Você precisa informar um nome de usuário',
-      'username.unique': 'O nome de usuário informado já existe',
-      'email.required': 'Você deve informar um endereço de email.',
-      'email.email': 'O email informado está num formato inválido.',
-      'email.unique': 'O email informado já existe.',
-      'password.required': 'Você precisa informar uma senha'
-    }
-  }
-  */
-
   get messages () {
     return Antl.list('validation')
   }
